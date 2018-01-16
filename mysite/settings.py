bb55fb30439783e91c7b5061a2abef70953f110e
admin <admin@sample.com>
2018-01-17 08:40:13 +0900

    database

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,7 +121,7 @@ USE_TZ = False
 STATIC_URL = '/static/'
 
 import dj_database_url
-#DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 

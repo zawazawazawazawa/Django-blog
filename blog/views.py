a5982f31a6065d9c1761296a906899c3f35ce66a
admin <admin@sample.com>
2018-02-04 21:21:45 +0900

    'comment'

--- blog/views.py
+++ blog/views.py
@@ -18,11 +18,8 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    try :
-        comments = Comment.objects.filter(published_date__lte=timezone.now()).order_by('published_date').filter(pk=pk)
-        return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments})
-    except ObjectDoesNotExist:
-        return render(request, 'blog/post_detail.html', {'post': post})
+    comments = Comment.objects.filter(published_date__lte=timezone.now()).order_by('published_date').filter(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments})
 
 def post_new(request):
     if request.method == "POST":

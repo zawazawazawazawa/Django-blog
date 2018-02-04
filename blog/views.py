881a99f6fcb66fffd1becbed8c4e2295631a4975
admin <admin@sample.com>
2018-02-04 21:39:32 +0900

    'slice'

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ import sys, os
 UPLOADE_DIR = os.path.dirname(os.path.abspath(__file__)) + '/static/files/'
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date').reverse()
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
